2f6700cc0c54556580aa43a5140646bd216325cb
Ma <maria.olivo.oliveira@escola.pr.gov.br>
2025-06-18 12:13:57 -0300

    Add files via upload

---
+++ Mafe 2B/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    '⏳ História',
+    'O que foi o pacto de Não-Agressão entre a URSS e a Alemanha?',
+    'Foi um acordo onde ambos os países se comprometiam a não atacar um ao outro por um período de 10 anos. Além da promessa de dividir a Europa Oriental entre ambos'
+)
+
+criaCartao(
+    '🌎 Geografia',
+    'Porque não há terremotos no Brasil?',
+    'Por causa de sua localização central na placa tectônica sul-americana, longe das zonas de contato entre placas.'
+)
+
+criaCartao(
+    '📚 Português',
+    'O que é um texto dissertativo-argumentativo?',
+    'É um tipo de texto em que o autor apresenta um ponto de vista sobre um determinado tema e o defende utilizando argumentos e evidências.'
+)
+
+criaCartao(
+    '🧪 Química',
+    'O que são reações exotérmicas?',
+    'São reações químicas em que a energia dos produtos é menor do que a energia dos reagentes, resultando em uma liberação de energia na forma de calor.'
+)
